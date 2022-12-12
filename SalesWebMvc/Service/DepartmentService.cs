28f28f52fa738133b9da1d47b43eac0c7d0b592a
danielribeiroDev <danieldiasstudent@gmail.com>
2022-12-12 08:51:33 -0300

    Init

---
+++ SalesWebMvc/Service/DepartmentService.cs
@@ -0,0 +1,24 @@
+﻿using SalesWebMvc.Data;
+using SalesWebMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Service
+{
+    public class DepartmentService
+    {
+        private readonly SalesWebMvcContext _context;
+
+        public DepartmentService(SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+
+        public ICollection<Department> FindAll()
+        {
+            return _context.Department.OrderBy(x => x.Name).ToList();
+        }
+    }
+}
